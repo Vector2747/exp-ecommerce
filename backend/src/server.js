ec717e20637e7a2446ae1325bc6627bd5b394f2c
Vector2747 <zakaria.tafoukt@se.univ-bejaia.dz>
2026-02-08 20:45:16 +0100

    clerk fonctionne (normalement)

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,20 @@
 import express from "express";
 import path from "path";
 import { ENV } from "./config/env.js"
+import { connectDB } from "./config/db.js";
+import { clerkMiddleware } from '@clerk/express'
+
+import { serve } from "inngest/express"
+import { functions, ingest} from "./config/ingest.js";
 
 const app = express();
 
 const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(clerkMiddleware());//  clerkMiddleware() est un middleware qui permet de vérifier si l'utilisateur est connecté ou pas. Si l'utilisateur est connecté, il ajoute les informations de l'utilisateur dans la requête (req.user) et passe à la suite. Si l'utilisateur n'est pas connecté, il renvoie une erreur 401 (Unauthorized).
+
+app.use("/api/ingest", serve({client:ingest, functions:functions}))//  serve() est une fonction qui permet de créer une route pour les fonctions Inngest. Elle prend en paramètre l'instance Inngest et un tableau de fonctions. Elle crée une route /api/ingest qui écoute les événements et exécute les fonctions correspondantes.
 
 app.get("/api/calling", (req,res) =>{
     res.status(200).json({message: "succes"})// donc le statut 200 veux dire succes et 500 echec ?
@@ -19,4 +29,7 @@ if(ENV.NODE_ENV === "production"){
     })
 }
 
-app.listen(ENV.PORT, () => console.log(ENV.NODE_ENV+ENV.PORT+"Le serveur roule ma boule !"));
+app.listen(ENV.PORT, () => {
+    console.log(ENV.NODE_ENV+ENV.PORT+"Le serveur roule ma boule !")
+    connectDB();
+});

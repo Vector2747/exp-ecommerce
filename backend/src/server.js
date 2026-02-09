383b28d828592f304dc682adb2202fe9fe1ed163
Vector2747 <zakaria.tafoukt@se.univ-bejaia.dz>
2026-02-09 14:43:18 +0100

    encore

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,9 +12,12 @@ const app = express();
 const __dirname = path.resolve();
 
 app.use(express.json());
-app.use(clerkMiddleware());//  clerkMiddleware() est un middleware qui permet de vérifier si l'utilisateur est connecté ou pas. Si l'utilisateur est connecté, il ajoute les informations de l'utilisateur dans la requête (req.user) et passe à la suite. Si l'utilisateur n'est pas connecté, il renvoie une erreur 401 (Unauthorized).
 
 app.use("/api/ingest", serve({client:ingest, functions:functions}))//  serve() est une fonction qui permet de créer une route pour les fonctions Inngest. Elle prend en paramètre l'instance Inngest et un tableau de fonctions. Elle crée une route /api/ingest qui écoute les événements et exécute les fonctions correspondantes.
+
+app.use(clerkMiddleware());//  clerkMiddleware() est un middleware qui permet de vérifier si l'utilisateur est connecté ou pas. Si l'utilisateur est connecté, il ajoute les informations de l'utilisateur dans la requête (req.user) et passe à la suite. Si l'utilisateur n'est pas connecté, il renvoie une erreur 401 (Unauthorized).
+
+// app.use("/api/ingest", serve({client:ingest, functions:functions}))//  serve() est une fonction qui permet de créer une route pour les fonctions Inngest. Elle prend en paramètre l'instance Inngest et un tableau de fonctions. Elle crée une route /api/ingest qui écoute les événements et exécute les fonctions correspondantes.
 
 app.get("/api/calling", (req,res) =>{
     res.status(200).json({message: "succes"})// donc le statut 200 veux dire succes et 500 echec ?

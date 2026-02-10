2ee1e61e43be352707ff3a8bd0afb1d18bce7b63
Vector2747 <zakaria.tafoukt@se.univ-bejaia.dz>
2026-02-10 18:00:54 +0100

    les routes et controlleurs de l'utilisateur ajoute

---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import { addAddress } from "../controlleurs/user.controlleur.js";
+import { protectRoute } from "../dingleware/auth.dingleware";
+import { getAddresses } from "../controlleurs/user.controlleur.js";
+import { updateAddress } from "../controlleurs/user.controlleur.js";
+import { deleteAddress } from "../controlleurs/user.controlleur.js";
+import { addToWishlist } from "../controlleurs/user.controlleur.js";
+import { getWishlist } from "../controlleurs/user.controlleur.js";
+import { deleteWishlist } from "../controlleurs/user.controlleur.js";
+
+const router = Router();
+
+router.use(protectRoute)
+
+// les routes d'addresses
+router.post("/addresses", addAddress);
+router.get("/addresses", getAddresses);
+router.put("/addresses/:addressId", updateAddress);
+router.delete("/addresses/:addressId", deleteAddress);
+
+// les routes de wishlist
+
+router.post("/wishlist", addToWishlist);
+router.get("/wishlist/:productId", getWishlist);
+router.delete("/wishlist/:productId", deleteWishlist);
+
+export default router;

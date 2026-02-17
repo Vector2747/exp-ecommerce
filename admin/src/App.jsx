b2ce4c39f74027775909b1398bdf39e401101f59
Vector2747 <zakaria.tafoukt@se.univ-bejaia.dz>
2026-02-17 14:29:41 +0100

    admin dashboard setup

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,19 +1,56 @@
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/clerk-react';
+import { SignedIn, SignedOut, SignInButton, SignUpButton, useAuth, UserButton } from '@clerk/clerk-react';
+import axiosInstance from './lib/axios.js';
+import { Routes, Route, Navigate} from 'react-router';
+import LoginPage from './pages/LoginPage.jsx';
+import DashboardPage from './pages/DashboardPage.jsx';
+import ProductsPage from './pages/ProductsPage.jsx';
+import OrdersPage from './pages/OrdersPage.jsx';
+import CustomersPage from './pages/CustomersPage.jsx';
+import DashboardLayout from './layouts/DashboardLayout.jsx';
+import { Divide, LoaderIcon } from 'lucide-react';
+import PageLoader from './components/PageLoader.jsx';
+//import { LoaderIcon } from 'lucide-react';
 
 function App() {
+  const getProducts = async () => {
+    const res =await axiosInstance.get("/products");
+    return res.data;
+  }
+
+  const { isSignedIn, isLoaded } = useAuth();
+
+  if(/*true*/!isLoaded) {
+    return <PageLoader />
+
+  };
   return (
-    <div>
-      <h1>App</h1>
-      {/* Show the sign-in and sign-up buttons when the user is signed out */}
+    /*<div>
+      <h1 className='text-red-500 text-3xl'>App</h1>
+      <button className='btn btn-primary'>
+        click me
+      </button>
+
+      {/* Show the sign-in and sign-up buttons when the user is signed out *}
       <SignedOut>
         <SignInButton mode="modal" />
         <SignUpButton mode="modal" />
       </SignedOut>
-      {/* Show the user button when the user is signed in */}
+      {/* Show the user button when the user is signed in *}
       <SignedIn>
         <UserButton />
       </SignedIn>
-    </div>
+    </div>*/
+    <Routes>
+      <Route path="/login" element={isSignedIn ? <Navigate to={"/dashboard"} /> : < LoginPage />} />
+      <Route path="/" element={isSignedIn ? <DashboardLayout /> : <Navigate to={"/login"} />}>
+        <Route index element={<Navigate to={"dashboard"} />} />
+        <Route path="dashboard" element={<DashboardPage />} />
+        <Route path="products" element={<ProductsPage />} />
+        <Route path="orders" element={<OrdersPage />} />
+        <Route path="customers" element={<CustomersPage />} />
+
+      </Route>
+    </Routes>
     
   )
 }
--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -2,6 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router'
+
+import {
+  /*useQuery,
+  useMutation,
+  useQueryClient,*/
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
 import { ClerkProvider } from '@clerk/clerk-react'
 
@@ -15,10 +24,17 @@ if (!PUBLISHABLE_KEY) {
   throw new Error('Add your Clerk Publishable Key to the .env file')
 }
 
+// Create a client
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-    <App />
-    </ClerkProvider>
+    <BrowserRouter>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </ClerkProvider>
+    </BrowserRouter>
   </StrictMode>,
 )

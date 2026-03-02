906776c47b50f548cdb7adc09e6036aa40ed816c
Vector2747 <zakaria.tafoukt@se.univ-bejaia.dz>
2026-03-02 16:16:42 +0100

    ca et aussi une mod des "Alert.alert" car moche

---
+++ mobile/app/(profile)/privacy-security.tsx
@@ -0,0 +1,14 @@
+import SafeScreen from "@/components/SafeScreen";
+import { View,Text } from "react-native";
+
+const PrivacyAndSecurityScreen = () => {
+    return (
+        <SafeScreen>
+            <Text className="text-white">
+                PrivacyAndSecurityScreen
+            </Text>
+        </SafeScreen>
+    )
+}
+
+export default PrivacyAndSecurityScreen;
